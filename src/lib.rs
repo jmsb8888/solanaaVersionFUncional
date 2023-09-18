99e7a401daf42608b66b5680befd561a52c0586b
Jose Salamanca <jose.salamanca03@uptc.edu.co>
2023-09-17 20:01:42 -0500

    proyecto en 0

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@ use solana_program::{
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct GreetingAccount {
     /// number of greetings
-    pub random_number: u32,
+    pub counter: u32,
 }
-use rand::Rng;
+
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
@@ -37,23 +37,13 @@ pub fn process_instruction(
         msg!("Greeted account does not have the correct program id");
         return Err(ProgramError::IncorrectProgramId);
     }
-// Generar un número aleatorio
-let mut rng = rand::thread_rng();
-let random_number = rng.gen_range(1..=100); // Número aleatorio entre 1 y 100
 
-// Increment and store the number of times the account has been greeted
-let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    // Increment and store the number of times the account has been greeted
+    let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    greeting_account.counter += 1;
+    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
-// Asignar el valor aleatorio a greeting_account.random_number
-greeting_account.random_number = random_number;
-
-greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
-msg!("Número aleatorio generado: {}", greeting_account.random_number);
-
-
-/*    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
-msg!("Número aleatorio generado: {}", greeting_account.counter);*/
-   // msg!("Greeted {} time(s)!", greeting_account.counter);
+    msg!("Greeted {} time(s)!", greeting_account.counter);
 
     Ok(())
 }
